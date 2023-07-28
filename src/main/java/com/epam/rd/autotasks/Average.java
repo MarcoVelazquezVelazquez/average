7c25415c31d8081ab864d821f1b279f40bc48ee4
Marco Antonio Velázquez Velázquez <velazquezv.marco@gmail.com>
2023-07-27 20:00:59 -0600

    Solved

--- src/main/java/com/epam/rd/autotasks/Average.java
+++ src/main/java/com/epam/rd/autotasks/Average.java
@@ -6,7 +6,18 @@ public class Average {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // Use Scanner methods to read input
+        int count = 0;
+        int average = 0;
 
+        while (true) {
+            int number = scanner.nextInt();
+            if (number == 0) {
+                break;
+            }
+            average = average + number;
+            count++;
+        }
+        System.out.println(average / count);
     }
 
 }
